92aed8125d069e578065eadc415baba36de6508d
许庭淞 <318715498@qq.com>
2017-06-26 08:15:40 +0800

    1.add block rule

---
+++ S8Blocker/ADBlock-iOS/ContentBlockerRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ContentBlockerRequestHandler.swift
+//  ADBlock-iOS
+//
+//  Created by virus1994 on 2017/6/26.
+//  Copyright © 2017年 ascp. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
+        
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+        
+        context.completeRequest(returningItems: [item], completionHandler: nil)
+    }
+    
+}
